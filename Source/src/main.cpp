72d1a6089917da07342725867d82ce6eedc0e5ea
Ripred100 <60947523+Ripred100@users.noreply.github.com>
2022-03-30 19:15:21 -0400

    SMALL

--- Source/src/main.cpp
+++ Source/src/main.cpp
@@ -1,39 +1,42 @@
 #include <Arduino.h>
 
-#define keypad
-//#define door_sensor
+#define KEYPAD
+//#define DOOR_SENSOR
 // Define other nodes to be made
 
-#ifdef keypad
-  #include 'keypad.h'
+#ifdef KEYPAD
+#include <Touch.h>
 #endif
 
+#ifdef DOOR_SENSOR
 
-void setup() {
+#endif
+
+void setup()
+{
   // To run once
-  
+
   // ------------------------------- \\
   // Define network SSID and PASS
   // Define GPIO pins to read from
   // Run setPass()
-  
 }
 
-void loop() {
+void loop()
+{
   // To run repetedly
 
   // if (keypad)
-    // ------------------------------- \\
+  // ------------------------------- \\
     // Check for addNode()
-    // > Make a connection 
+  // > Make a connection
 
-    // Check for enableSys()
-    // > Set a flag to enable some stuff
+  // Check for enableSys()
+  // > Set a flag to enable some stuff
 
-    // Check for disableSys()
-    // > Set a flag to not check stuff
-    
-    // Check for removeNode()
-    // > Remove the node that is pinging some specific thing
+  // Check for disableSys()
+  // > Set a flag to not check stuff
 
+  // Check for removeNode()
+  // > Remove the node that is pinging some specific thing
 }
