a913da0b54dfac23f189aa2047fa185286df3b6b
jmutton2@uwo.ca <jmutton2@uwo.ca>
2022-03-31 18:53:35 -0400

    More framework

--- Source/src/main.cpp
+++ Source/src/main.cpp
@@ -15,60 +15,83 @@
 
 #endif
 
-const int SYS_STATUS = 26; // <<< CHANGE THIS TO WHATEVER PIN THE KEYPAD IS CONNECTION TO
+// Acts as shared memory
+const int PASSWORD_STATUS = 20;
+const int RING = 21;
+const int COUNTDOWN = 22;
+const int DISABLE_SYS_KEY = 23;
+const int ENABLE_SYS_KEY = 24;
+const int ENABLE_NETWORK_PEERS = 25;
+const int RESET_ALARM_KEY = 26;
 const int ALERT_INTERRUPT = 27;
 const int RAISE_ALARM_INTERRUPT = 28;
 
-int countdown = 0;
-int sys_enabled = 0;
+int sys_enabled;
 
 void setup()
 {
-  Server_Init();
+  sys_enabled = 1;
 
-  // if (keypad)
+  // if (HIGH_POWER)
+  Server_Init();
+  // if (ENABLE_NETWORK_PEERS) >> Peer_Init()
+
+  // if (KEYPAD)
   // Run Set_Pass()
 }
 
 void loop()
 {
-  attachInterrupt(SYS_STATUS, CHANGE_SYS_STATUS, CHANGE);
+  // Interrupt to enable nad disable the system
+  // Triggered by change to system rising/falling
+  // Could have two pins or just one (ENABLE_SYS_KEY and DISABLE_SYS_KEY)
+  // attachInterrupt(DISABLE_SYS_KEY, DISABLE_SYS, FALLING);
+  attachInterrupt(ENABLE_SYS_KEY, ENABLE_SYS, RISING);
 
-  // if (keypad)
-  attachInterrupt(ALERT_INTERRUPT, AWAIT_ALARM, HIGH);
-  attachInterrupt(RAISE_ALARM_INTERRUPT, RAISE_ALARM, HIGH);
+  // Handles turning off the alarm
+  attachInterrupt(RESET_ALARM_KEY, RESET_ALARM, RISING);
 
-  // Countdown to check for timer
-  if (countdown)
+  // Sets the pin to ask for a password
+  // Checks a password correct pin or password incorrect pin
+  attachInterrupt(ALERT_INTERRUPT, AWAIT_ALARM, RISING);
+  attachInterrupt(RAISE_ALARM_INTERRUPT, RAISE_ALARM, RISING);
+
+  if (COUNTDOWN)
   {
-    // Timer decrease
-    // if (password == saved password)
-    // digitalWrite(ALERT_INTERRUPT, LOW);
-
-    // if (password !== saved password)
-    //  digitalWrite(RAISE_ALARM_INITERRUPT, HIGH);
+    // delay(10000) << Wait for 10 seconds
+    attachInterrupt(PASSWORD_STATUS, CHECK_PASS, HIGH);
+    attachInterrupt(PASSWORD_STATUS, RESET_ALARM, LOW);
   }
-
-  // Check for Remove_Node()
-  // > Check for close packet
-  // > Send close packet
-  // > Close the connection that sent close packet
-
-  // Check for Close_Connection()
-  // > Send a request to close packet
-  // > Await response and close if close packet received
 }
 
-// Enable or Disable the system
-void CHANGE_SYS_STATUS()
+// Arms the system
+void ENABLE_SYS()
 {
-  sys_enabled = !sys_enabled;
+  sys_enabled = 1;
+}
+
+// Disarms the system
+void DISABLE_SYS()
+{
+  sys_enabled = 1;
+}
+
+// Turns the alarms off
+void RESET_ALARM()
+{
+  digitalWrite(ALERT_INTERRUPT, LOW);
+  digitalWrite(RAISE_ALARM_INTERRUPT, LOW);
 }
 
 // Trigger the alarm countdown noise
+// Start
 void AWAIT_ALARM()
 {
-  countdown = 1;
+  digitalWrite(COUNTDOWN, HIGH);
+}
+
+void CHECK_PASS()
+{
 }
 
 // Trigger the alarm
