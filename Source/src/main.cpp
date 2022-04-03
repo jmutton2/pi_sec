f97a0b51cfe09a775eefb4eff761bd96452a4271
jmutton2@uwo.ca <jmutton2@uwo.ca>
2022-04-03 19:36:04 -0400

    Framework with states adn iffs

--- Source/src/main.cpp
+++ Source/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 // DEFINE THE DEVICE
-//#define KEYPAD
+#define KEYPAD
 //#define DOOR_SENSOR
 //#define CAMERA
 //#define AC_SENSOR
@@ -46,12 +46,48 @@
 // SETUP
 void setup()
 {
+    //##IF DEFINED HP_DEVICE
+    // Start HP Server
+    // Note >> This needs to connect to other peers
+
+    //##IF DEFINED LP_DEVICE
+    // Start LP Server
+
+    //##If DEFINED KEYPAD
+    // Create Password
+
+    // Enable System
+    // Note >> This needs to be relayed to other peers
 }
 // -------------------
 
 // LOOP
 void loop()
 {
+
+    //##IF DEFINED HP_DEVICE
+    //#STATE: ARMED
+    // Check for UNARM interrupt
+    // Check for AWAIT_ALARM interrupt
+
+    //#STATE: UNARMED
+    // Check for ARM interrupt
+
+    //##IF DEFINED LP_DEVICE
+
+    //##If DEFINED KEYPAD
+    //#STATE: AWAITING ALARM
+    // Check for password correctness
+    // FALSE => set_state to ALARM_RINGING
+    // TRUE => set_state to UNARMED
+
+    //#STATE: ALARM_RINGING
+    // Check for password correctness
+    // TRUE => set_state to UNARMED
+
+    //##IF DEFINED DOOR_SENSOR
+    // Check for DOOR_STATE_CHANGED interrupt
+    // Alert (POST) to parent
 }
 // -------------------
 
