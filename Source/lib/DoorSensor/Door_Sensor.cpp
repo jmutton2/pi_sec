7836f289f015ca8a73fc8f9d003b6ce29bf583a3
jmutton2 <jmutton2@uwo.ca>
2022-04-09 11:58:51 -0400

    Fixed multiple send_alert bug

--- Source/lib/DoorSensor/Door_Sensor.cpp
+++ Source/lib/DoorSensor/Door_Sensor.cpp
@@ -7,7 +7,7 @@
 void Door_Sensor_Loop(Control *mem)
 {
     // Check for DOOR_STATE_CHANGED interrupt
-    if (touchRead(touchGPIO) > 50)
+    if (touchRead(touchGPIO) > 50 && mem->system_state != 4 && mem->system_state != 5)
     {
         Send_Alert();
         AWAIT_ALARM(mem);
